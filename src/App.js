8ef64275cb1e73903ca4b7257ebe0f71df17f2b2
wpanumas1980 <wpanumas@gmail.com>
2021-01-20 09:53:46 +0700

    basic react router 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import './App.css';
+
+const Home = () => <div>Home Page</div>
+const About = () => <div>About Page</div>
+const User = () => <div>User Page</div>
+const NotFound = () => <div>NotFound Page</div>
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav style={{ width: '60%', margin: '50px auto' }}>
+        <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <div style={{ width: '60%', margin: '50px auto' }}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/:user">
+            <User />
+          </Route>
+          <Route >
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
